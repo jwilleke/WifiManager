9069afbbc77d7caf02c094a2f648fecb5d03486d
Jim Willeke <jim@willeke.com>
2024-01-26 03:18:35 -0500

    Documentation and cleanup

--- src/WiFiManager.h
+++ src/WiFiManager.h
@@ -1,5 +1,10 @@
-// WiFiManager.h
-// Description: WiFiManager class header file
+
+
+/**
+ * @file WiFiManager.h
+ * @brief This file contains the declaration of the WiFiManager class.
+ */
+
 #ifndef WiFiManager_h
 #define WiFiManager_h
 
@@ -8,22 +13,46 @@
 #include <WiFiUdp.h>
 #include <WiFiClient.h>
 
-
+/**
+ * @class WiFiManager
+ * @brief The WiFiManager class provides functionality for managing WiFi connections and time synchronization.
+ */
 class WiFiManager {
 public:
+  /**
+   * @brief Constructs a WiFiManager object with the specified SSID and password.
+   * @param ssid The SSID of the WiFi network.
+   * @param password The password of the WiFi network.
+   */
   WiFiManager(const char* ssid, const char* password);
-  void connect();
-  void syncTime();
-  unsigned long getCurrentTime();
-  void printWiFiStatus(); // Add a public member function to print the WiFi status
 
+  /**
+   * @brief Connects to the WiFi network using the specified SSID and password.
+   */
+  void connect();
+
+  /**
+   * @brief Synchronizes the device's time with an NTP server.
+   */
+  void syncTime();
+
+  /**
+   * @brief Gets the current time in milliseconds since the epoch.
+   * @return The current time in milliseconds.
+   */
+  unsigned long getCurrentTime();
+
+  /**
+   * @brief Prints the WiFi connection status to the serial monitor.
+   */
+  void printWiFiStatus();
 
 private:
-  const char* ssid;
-  const char* password;
-  WiFiClient wifiClient;
-  NTPClient timeClient;
-  WiFiUDP wifiUDP; // Add a private member variable of type WiFiUDP to the WiFiManager class
+  const char* ssid; /**< The SSID of the WiFi network. */
+  const char* password; /**< The password of the WiFi network. */
+  WiFiClient wifiClient; /**< The WiFi client object for managing the connection. */
+  NTPClient timeClient; /**< The NTP client object for time synchronization. */
+  WiFiUDP wifiUDP; /**< The UDP object for NTP communication. */
 };
 
 #endif
